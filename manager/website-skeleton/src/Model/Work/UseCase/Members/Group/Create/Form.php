15dc0e18eea73343ddd003c4da33dfb241b7b163
ilij <ilij@ya.ru>
2020-03-13 02:35:39 +0300

    Add work member groups.

---
+++ manager/website-skeleton/src/Model/Work/UseCase/Members/Group/Create/Form.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Model\Work\UseCase\Members\Group\Create;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('name',Type\TextType::class);
+
+
+	}
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array(
+			'data_class'=>Command::class,
+		));
+	}
+
+}
